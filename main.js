d824957ffeb26921fa7dd4b0cb7ad7943d6c6584
Kiril Tucker <kiril.tucker@gmail.com>
2022-04-10 13:29:33 +0200

    Double curly bracket syntax

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: 'Jacquard',
+      description: 'Simply put, jacquard is a specially woven fabric created using a Jacquard loom and various materials such as cotton, polyester, silk and acrylic can be woven to create them.'
+    }
+  }
+})
